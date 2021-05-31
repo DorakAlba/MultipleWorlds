cebd571b903f82e87184c419db98855a2c63e31e
DorakAlba <mr.demyank@gmail.com>
2021-05-31 20:23:37 +0300

    Crossover

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,13 +3,18 @@ import random as rd
 
 class Population:
     def __init__(self, population_size=8, total_stats=10):
-        #todo possily replace with dictionary and safe best results
+        # todo possily replace with dictionary and safe best results
         self.population = []
+        self.total_stats = total_stats
         for creature in range(population_size):
             self.population.append(CharacterStats(total_stats=total_stats))
         self.new_population = []
 
     def new_generation(self):
+        """
+        Create new population
+        :return:
+        """
         self.population = self.new_population
         self.new_population = []
 
@@ -17,9 +22,14 @@ class Population:
         pass
 
     def crossover(self, creature1, creature2):
-        #todo crossover
-        child1.mutate_stats
-        child2.mutate_stats
+        cutting_point = rd.randint(1, len(creature1))
+
+        child1 = creature1.stat_list()[0:cutting_point] + creature2.stat_list()[cutting_point:-1]
+        child2 = creature2.stat_list()[0:cutting_point] + creature1.stat_list()[cutting_point:-1]
+        child1 = CharacterStats(*child1, total_stats=self.total_stats)
+        child2 = CharacterStats(*child2, total_stats=self.total_stats)
+        child1.mutate_stats()
+        child2.mutate_stats()
         self.new_population.extend([child1, child2])
 
 
@@ -45,6 +55,12 @@ class CharacterStats:
             child[keys[0]] += 1
             child[keys[1]] -= 1
 
+    def stat_list(self):
+        stats = []
+        for key in self.stat_names:
+            stats.append(self.stat_names[key])
+        return stats
+
     def change_random_stat(self, increase=True):
         stat_to_raise = self.select_random_stat()
         if increase is True:
@@ -61,6 +77,10 @@ class CharacterStats:
             self.change_random_stat(increase=False)
 
     def calc_allocated_stats(self):
+        """
+        Calculate amount of free or borrowed stats
+        :return:
+        """
         self.free_stats = self.total_stats - self.calc_stat_sum()
 
     def calc_stat_sum(self):
@@ -78,55 +98,3 @@ class CharacterStats:
         print(self.stat_names)
 
 
-pop = Population(total_stats=100)
-for char in pop.population:
-    char.display_stats()
-#
-#     stats = [strength, dexterity, vitality, agility]
-#     for g in range(total):
-#         stats[random.randint(0, len(stats) - 1)] += 1
-#     return stats
-#
-# def initiate_population(number: int, total_stats: int):
-#     population = []
-#     for species in range(number):
-#         population.append(initiate_stats(total_stats))
-#     return population
-#
-# def mutate_stats(character_stats, probability=0.25):
-#     if random.random() < probability:
-#         character_stats[random.randint] += 1
-#         character_stats[random.randint] -= 1
-#     return character_stats
-#
-# def crossover(a, b):
-#     if random.random() <= 0.5:
-#         a, b = b, a
-#     child = []
-#     place_of_cut = len(a) // 2
-#     new_a = a[0:place_of_cut].copy()
-#     new_a_tail = a[place_of_cut:].copy()
-#     new_b = b[place_of_cut:].copy()
-#     mapping = {new_b[i]: new_a_tail[i] for i in range(len(new_a_tail))}
-#     for a1 in range(len(new_a)):
-#         while new_a[a1] in new_b:
-#             new_a[a1] = mapping[new_a[a1]]
-#
-#     # Here we gonna fix repetition of genes
-#     # child.extend(new_a)
-#     # child.extend(new_b)
-#     print(new_a)
-#     print(new_a)
-#     child = new_a + new_b
-#     # breeding for our solutions
-#     return child
-#
-# def crossover(a, b):
-#     perfect_child
-#     for parameter in range(len(a))
-#
-#     population = initiate_population(4, 5)
-#
-# print(population)
-# child = crossover(population[0], population[1])
-# print(child)
